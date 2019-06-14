521e54ade7620d6b1e43e1ea224c93a09c91cec7
Joshua Byler <bylerj@gmail.com>
2019-06-14 12:24:43 -0400

    got colors and all the fonts working

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="color-picker">
+    <div class="color"
+         v-for="color in colors"
+         v-bind:key="color"
+         v-bind:style="{ backgroundColor: color, }"
+         v-on:click="colorClick(color)"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColorPicker',
+  data() {
+    return {
+      colors: [
+        'rgb(10, 15, 23)',
+        'rgb(56, 56, 56)',
+        'rgb(24, 84, 23)',
+        'rgb(26, 57, 99)',
+        'rgb(64, 32, 85)',
+        'rgb(222, 158, 40)',
+        'rgb(224, 96, 15)',
+        'rgb(148, 29, 31)',
+        'rgb(73, 43, 35)',
+        'rgb(4, 4, 4)'],
+    };
+  },
+  methods: {
+    colorClick(color) {
+      this.$emit('handleColorClick', color);
+    },
+  },
+};
+</script>
---
+++ src/views/Picker.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="main">
+    <div class="text">
+      <input v-model="message" type="text" placeholder="try me">
+    </div>
+    <colorPicker
+      v-on:handleColorClick="handleColorClick"
+    ></colorPicker>
+    <div v-for="font in fonts" v-bind:key="font.name" class="font">
+      <p>{{font.name}}</p>
+      <p v-bind:style="{
+      fontFamily: font.name,
+      fontSize: fontSize + 'px',
+      color: activeColor }">{{message}}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+import colorPicker from '../components/ColorPicker.vue';
+
+export default {
+  name: 'picker',
+  components: {
+    colorPicker,
+  },
+  data() {
+    return {
+      activeColor: 'black',
+      fontSize: 30,
+      fonts: [
+        {
+          name: 'Righteous',
+        },
+        {
+          name: 'Open Sans Condensed',
+        },
+        {
+          name: 'Lobster',
+        },
+        {
+          name: 'Pacifico',
+        },
+        {
+          name: 'Shadows into Light',
+        },
+        {
+          name: 'Amatic',
+        },
+      ],
+      message: '',
+    };
+  },
+  methods: {
+    handleColorClick(color) {
+      this.activeColor = color;
+    },
+  },
+};
+</script>
