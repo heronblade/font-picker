8dfe97359e6d0b5a1c0cf96d2eaf0d9c2eb4968e
Joshua Byler <bylerj@gmail.com>
2019-06-14 14:35:16 -0400

    cleaned it up and added the correct icons

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/picker">Picker</router-link>
-    </div>
-    <router-view/>
+    <picker></picker>
   </div>
 </template>
+
+<script>
+import picker from './views/Picker';
+export default {
+  name: 'app',
+  components: {
+    picker,
+  },
+}
+</script>
 
 <style lang="scss">
   @import './assets/scss/main';
